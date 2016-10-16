3f29fcdaea5b8584e22fdeb29c8c70c4b548f369
Viper <viper666@inbox.ru>
2016-10-16 22:20:05 +0300

    Add login page and auth

--- MyFirstWebsite/Global.asax.cs
+++ MyFirstWebsite/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using MyFirstWebsite.App_Start;
 
 namespace MyFirstWebsite
 {
@@ -13,6 +14,7 @@ namespace MyFirstWebsite
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
         }
     }
 }
