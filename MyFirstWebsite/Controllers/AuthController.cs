a50a41aa2c4f54e12f4961709ceb772d5c247bee
Viper <viper666@inbox.ru>
2016-10-17 23:09:03 +0300

    identity claims

--- MyFirstWebsite/Controllers/AuthController.cs
+++ MyFirstWebsite/Controllers/AuthController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using MyFirstWebsite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Security.Claims;
 
 namespace MyFirstWebsite.Controllers
 {
@@ -10,9 +12,51 @@ namespace MyFirstWebsite.Controllers
     public class AuthController : Controller
     {
         // GET: Auth
-        public ActionResult Login()
+        [HttpGet]
+        public ActionResult Login(string returnUrl)
         {
-            return View();
+            var model = new LoginModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(model);
         }
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)//checks if the input fields have correct format
+            {
+                return View(model);//Returns the view with the input values so that the user doesn't have to retype again
+            }
+            //Checks whether the input is the same as those literals. Note: Never ever do this! This is just to demo the validation while we're not yet doing any database interaction
+            if (model.Email == "admin@admin.com" && model.Password == "123456")
+            {
+                var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, "Xtian"),
+                    new Claim(ClaimTypes.Email, "xtian@email.com"),
+                    new Claim(ClaimTypes.Country, "Philippines")
+
+                }, "ApplicationCoockie");
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+                authManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+            ModelState.AddModelError("", "Invalid email or password");
+            return View(model);
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+            return returnUrl;
+        }
+
     }
+    
 }
